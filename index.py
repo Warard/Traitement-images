25ef964c5c5a77eeea426998e9efb6b25e014668
Mathéo TROUILLE <55890393+Warard@users.noreply.github.com>
2021-06-10 07:34:14 +0200

    Correctifs mineurs (commentaire, lisibilité, opti)

--- index.py
+++ index.py
@@ -11,7 +11,7 @@
 def val_dec(n_octets):
     '''
     Convertit un code ASCII en valeur décimale. 
-    Ajouter b devant l'argument str n_octets
+    Rq : Ajouter b devant l'argument str n_octets pour préciser que c'est un ASCII et non un simple str
     arg : 
         b'str' n_octets : Code ASCII à convertir 
     return : 
@@ -19,11 +19,7 @@ def val_dec(n_octets):
     '''
     result = 0
     for i in range(len(n_octets)):
-        if i == 0:
-            result += n_octets[i]
-        else:
-            result += n_octets[i] * 256**i
-
+        result += n_octets[i] * 256**i
     return result
 
 
@@ -32,7 +28,7 @@ def val_dec(n_octets):
 def ouvrir_fichier(image_source, octets_to_read=0):
     '''
     Ouvre un fichier, et retourne son contenu en type byte
-    Penser à le convertir en bytearray pour modifier son contenu via bytearray()
+    Pour modifidier son contenu, le convertir en bytearray via bytearray()
     args :
         str image_source : Nom du fichier source et son extension
         int octets_to_read : Nombre total d'octets à lire. Si = 0 (par défaut), tout est lu
@@ -45,6 +41,7 @@ def ouvrir_fichier(image_source, octets_to_read=0):
     # Si il n'y a pas de valeurs spécifiques à lire 
     if octets_to_read == 0:
         data = source.read()
+    # Si il n'y a qu'un nombre octets_to_read à lire
     else:
         data = source.read(octets_to_read)
 
@@ -57,7 +54,7 @@ def ouvrir_fichier(image_source, octets_to_read=0):
 def info_image(image_source): 
     '''
     Affiche les infos suivantes sur l'image donnée en argument : 
-        signature, taille, champ reservé, offset, largeur et hauteur
+        signature, taille, champ reservé, offset, largeur, hauteur et codage de la couleur
     arg : 
         str image : nom et extension du fichier image source
     return : 
@@ -69,7 +66,7 @@ def info_image(image_source):
     print("Signature : ", infos[0:2])
     print("Taille du fichier: ", val_dec(infos[2:6]), "octets")
     print("Champ réservé :", val_dec(infos[6:10]))
-    print("Offset de l'image :", val_dec(infos[10:14]))
+    print("Offset de l'image :", val_dec(infos[10:14]), "octets")
     print("Largeur de l'image :", val_dec(infos[18:22]), "px")
     print("Hauteur de l'image :", val_dec(infos[22:26]), "px")
 
@@ -80,18 +77,16 @@ def info_image(image_source):
 #----- CONVERSION IMAGE EN ROUGE -----#
 def fonction_vers_rouge(image_source, image_finale):
     '''
-    Convertit une image colorée en une image contant uniquement sa composante rouge.
-    Le résultat est stockée dans image_finale 
+    Convertit une image colorée en une image contenant uniquement sa composante rouge.
+    Le résultat est stocké dans image_finale 
     args :
         str image_source : nom et extension de l'image initiale 
         str image_finale : nom et extension de l'image finale. cette valeur sera le nom du fichier image
     return : 
         void
     '''
-    datas = ouvrir_fichier(image_source)
-    
+    datas = bytearray(ouvrir_fichier(image_source))
     offset = val_dec(datas[10:14])
-    datas = bytearray(datas) 
 
     for i in range(offset, len(datas), 3):
         datas[i] = 0
@@ -101,25 +96,21 @@ def fonction_vers_rouge(image_source, image_finale):
     fichier_final.write(datas)
     fichier_final.close()
 
-# vers_rouge()
-
 
 
 #----- INVERSION D'IMAGE -----#
 def fonction_inversion_image(image_source, image_finale):
     '''
     Inverse les couleurs d'une image 
-    Le résultat est stockée dans image_finale 
+    Le résultat est stocké dans image_finale 
     args :
         str image_source : nom et extension de l'image initiale 
         str image_finale : nom et extension de l'image finale. cette valeur sera le nom du fichier image
     return : 
         void
     '''
-    datas = ouvrir_fichier(image_source)
-    
+    datas = bytearray(ouvrir_fichier(image_source))
     offset = val_dec(datas[10:14])
-    datas = bytearray(datas) 
 
     for i in range(offset, len(datas)):
         datas[i] = 255 - datas[i] 
@@ -128,24 +119,20 @@ def fonction_inversion_image(image_source, image_finale):
     fichier_final.write(datas)
     fichier_final.close()
 
-# inversion_image()
-
 
 
 #----- NOIR ET BLANC -----#
 def fonction_noir_et_blanc(image_source, image_finale):
     '''
     Transforme une image colorée en une image en noir et blanc 
-    Le résultat est stockée dans image_finale 
+    Le résultat est stocké dans image_finale 
     args :
         str image_source : nom et extension de l'image initiale 
         str image_finale : nom et extension de l'image finale. cette valeur sera le nom du fichier image
     return : 
         void
     '''
-    datas = ouvrir_fichier(image_source)
-    datas = bytearray(datas)
-
+    datas = bytearray(ouvrir_fichier(image_source))
     offset = val_dec(datas[10:14])
     
     for i in range(offset, len(datas)):
@@ -153,15 +140,15 @@ def fonction_noir_et_blanc(image_source, image_finale):
         v = val_dec(datas[i+1:i+2])
         b = val_dec(datas[i:i+1])
 
+        # Calcul moyenne des pixels Rouge, Vert, Bleu
         m = (r+v+b) // 3
 
+        # Insertion de la moyenne des 3 pixels sur chaque pixel
         datas[i] = m 
 
     fichier_final = open(image_finale, 'wb')
     fichier_final.write(datas)
     fichier_final.close()
-
-# noir_et_blanc() 
 
 
 
@@ -169,15 +156,14 @@ def fonction_noir_et_blanc(image_source, image_finale):
 def fonction_monochrome(seuil, niveau1, niveau2, image_source, image_finale):
     '''
     Transforme une image en noir et blanc 3 couleurs, en une image monochrome 
-    Le résultat est stockée dans image_finale 
+    Le résultat est stocké dans image_finale 
     args :
         str image_source : nom et extension de l'image initiale (doit-être en noir et blanc)
         str image_finale : nom et extension de l'image finale. cette valeur sera le nom du fichier image
     return : 
         void
     '''
-    datas = ouvrir_fichier(image_source)
-    datas = bytearray(datas)
+    datas = bytearray(ouvrir_fichier(image_source))
 
     offset = val_dec(datas[10:14])
     
@@ -209,8 +195,6 @@ def fonction_monochrome(seuil, niveau1, niveau2, image_source, image_finale):
     fichier_final.write(datas)
     fichier_final.close()
 
-# monochrome(seuil = 90, niveau1 = 50, niveau2 = 180)
-
 
 
 #----- ROTATION 180 -----#
@@ -229,15 +213,14 @@ def fonction_rotation_180_degres(image_source, image_finale):
     largeur = val_dec(datas[18:22])
     offset = val_dec(datas[10:14])
     
-    # Données de l'image finale ; ici, ajout du header provenant de l'image source 
+    # Données de l'image de DESTination ; ici, ajout du header provenant de l'image source 
     datas_dest = bytearray(datas[0:offset])
     
-    # On réouvre le fichier source de manière à utiliser .seek pour lire ses lignes
+    # On réouvre le fichier source pour avoir accès à la méthode .seek() qui lira ses lignes
     src = open(image_source, 'rb')
 
     # Initialisation de la variable qui contiendra les copies des lignes
     ligne = []
-
 
     for i in range(1, hauteur+1):
         # Parcours les lignes unes à une en partant de la fin et les stocke (partir de la fin permet de faire la rotation)
@@ -247,14 +230,11 @@ def fonction_rotation_180_degres(image_source, image_finale):
         # Ecris les lignes unes à unes
         for k in range(len(ligne)):
             datas_dest.append(ligne[k])
-
         
 
     fichier_final = open(image_finale, 'wb')
     fichier_final.write(datas_dest)
     fichier_final.close()
-
-# rotation_180() 
 
 
 
@@ -274,10 +254,10 @@ def fonction_miroir(image_source, image_finale):
     largeur = val_dec(datas[18:22])
     offset = val_dec(datas[10:14])
     
-    # Données de l'image finale ; ici, ajout du header provenant de l'image source 
+    # Données de l'image de DESTination ; ici, ajout du header provenant de l'image source 
     datas_dest = bytearray(datas[0:offset])
     
-    # On réouvre le fichier source de manière à utiliser .seek pour lire ses lignes
+    # On réouvre le fichier source pour avoir accès à la méthode .seek() qui lira ses lignes
     src = open(image_source, 'rb')
 
     # Initialisation de la variable qui contiendra les copies des lignes
@@ -285,13 +265,12 @@ def fonction_miroir(image_source, image_finale):
 
     # Parcours toute la hauteur de l'image
     for i in range(hauteur):
-        # Parcours les lignes unes à une en partant de la fin et les stocke (partir de la fin permet de faire la rotation)
+        # Parcours les lignes unes à unes
         src.seek(offset+(i*largeur*3))
         ligne = src.read(largeur*3)
 
         # Ecris les lignes unes à unes, en partant de la fin via le sclicing
         for k in range(0, len(ligne), 3):
-            # print(k)
             datas_dest.append(ligne[::-1][k+2])  #bleu
             datas_dest.append(ligne[::-1][k+1])  #vert
             datas_dest.append(ligne[::-1][k])    #rouge  
